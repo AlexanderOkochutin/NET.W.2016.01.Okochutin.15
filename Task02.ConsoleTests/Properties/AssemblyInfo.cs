659c0602eac16c0a57142ce65a1a9e133c826472
AlexanderOkochutin <okochutinwork@gmail.com>
2016-11-27 15:04:22 +0300

    fix incorrect names

--- Task02.ConsoleTests/Properties/AssemblyInfo.cs
+++ Task02.ConsoleTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Task02.ConsoleTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("Task02.ConsoleTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f47b2f1-7c97-4fac-bcda-7c9d47035a95")]
+[assembly: Guid("391c900c-6b1d-44f7-830c-3e3765bf9b23")]
 
 // Version information for an assembly consists of the following four values:
 //
